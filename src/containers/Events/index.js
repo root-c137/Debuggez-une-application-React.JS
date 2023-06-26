b5d8eda41df0c82e08b90a9b5c92b066bc65cbb0
rootc137 <pessicksanchez@gmail.com>
2023-06-26 16:05:22 +0200

    fix currentPage bold..

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -12,6 +12,7 @@ const PER_PAGE = 9;
 const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
+  const classCurrentPage = "CurrentPage";
   const [currentPage, setCurrentPage] = useState(1);
 
   const filteredEvents = (
@@ -70,7 +71,10 @@ const EventList = () => {
           <div className="Pagination">
             {[...Array(pageNumber || 0)].map((_, n) => (
               // eslint-disable-next-line react/no-array-index-key
-              <a key={n} href="#events" onClick={() => setCurrentPage(n + 1)}>
+              <a key={n} href="#events" className={currentPage === (n+1) ? classCurrentPage : null}
+                 onClick={() => {
+                    setCurrentPage(n + 1);
+                 }}>
                 {n + 1}
               </a>
             ))}
